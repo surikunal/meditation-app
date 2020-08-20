188e1d2d3eb14bce29bb82387ad9ed58d00e712f
kunal <suri.kunal.222@gmail.com>
2020-08-21 00:17:18 +0530

    projeect completed

--- script.js
+++ script.js
@@ -8,17 +8,34 @@ const app = () => {
     const sounds = document.querySelectorAll('.sound-picker button');
     //Time display
     const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
     //get the length of the outline
     const outlineLength = outline.getTotalLength();
     //Duration
-    let fakeDuration = 600;
+    let fakeDuration = 60;
     outline.style.strokeDasharray = outlineLength;
     outline.style.strokeDashoffset = outlineLength;
 
+    // play a different song
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function() {
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        });
+    });
 
     //play songs
     play.addEventListener("click", () => {
         checkPlaying(song);
+    });
+
+    // select sound
+    timeSelect.forEach(option => {
+        option.addEventListener("click", function () {// we are using normal function here bcz we are going to use "this" keyword inside this function which is not possible for arrow function
+            fakeDuration = this.getAttribute("data-time");
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
+        });
     });
 
     // create a function specific to stop and play the sound
@@ -43,7 +60,18 @@ const app = () => {
 
         // we can animate the circle  
         let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
-        outline.style.strokeDashoffset = prgress;
+        outline.style.strokeDashoffset = progress;
+
+        // animate the time text
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        // to stop when time over
+        if (currentTime >= fakeDuration) {
+            song.pause();
+            video.pause();
+            play.src = "./svg/play.svg";
+            song.currentTime = 0;
+        }
     };
 };
 
